9cfcee823c45736a7f2d4e010c2965327fc7431d
stefaleon <stefaleon@gmail.com>
2019-12-11 18:13:43 +0200

    configure the postSignUp method

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,25 @@
 const User = require("../models/user");
 
-exports.postSignUp = (req, res, next) => {};
+exports.postSignUp = (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: "Email and Password are required" });
+  }
+
+  User.findOne({ email })
+    .then(existingUser => {
+      if (existingUser) {
+        return res.status(422).send({ error: "Email is in use" });
+      } else {
+        const user = new User({
+          email,
+          password
+        });
+        user.save().then(savedUser => res.json(savedUser));
+      }
+    })
+    .catch(err => next(err));
+};
 
 exports.postLogIn = (req, res, next) => {};
