c8f5b06d52939e8d7f068da8411caceb6f94a39a
stefaleon <stefaleon@gmail.com>
2019-12-11 19:38:02 +0200

    respond with a jwt on user sign up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,11 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
+
+const private = require("../private/private");
+
+const SECRET = private.jwtSecret;
 
 exports.postSignUp = (req, res, next) => {
   const { email, password } = req.body;
@@ -14,21 +19,33 @@ exports.postSignUp = (req, res, next) => {
       if (existingUser) {
         return res.status(422).send({ error: "Email is in use" });
       } else {
-        bcrypt.genSalt(10).then(salt => {
-          bcrypt.hash(password, salt).then(hashedPassword => {
-            const user = new User({
-              email,
-              password: hashedPassword
-            });
+        bcrypt
+          .genSalt(10)
+          .then(salt => {
+            bcrypt
+              .hash(password, salt)
+              .then(hashedPassword => {
+                const user = new User({
+                  email,
+                  password: hashedPassword
+                });
 
-            return user.save().then(savedUser => res.json(savedUser));
-          });
-        });
+                return user
+                  .save()
+                  .then(savedUser => {
+                    const token = jwt.sign({ userId: savedUser._id }, SECRET, {
+                      expiresIn: "1h"
+                    });
+                    res.json(token);
+                  })
+                  .catch(err => next(err));
+              })
+              .catch(err => next(err));
+          })
+          .catch(err => next(err));
       }
     })
-    .catch(err => {
-      next(err);
-    });
+    .catch(err => next(err));
 };
 
 exports.postLogIn = (req, res, next) => {};
